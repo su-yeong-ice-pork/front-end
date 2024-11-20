81f799cf493b9ed17e56ba70a02decdce3a5725b
taeyomi <terry11ty@naver.com>
2024-11-20 23:13:22 +0900

    refactor: Modal 폴더 생성하여 위치 인증 및 기능 추가 예정 모달 컴포넌트화

---
+++ src/components/Modal/UpcomingModal.tsx
@@ -0,0 +1,45 @@
+import {
+  Modal,
+  ModalBackdrop,
+  ModalContent,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+} from '@/components/ui/modal';
+import {Text} from 'react-native';
+import React from 'react';
+import {UpcomingModalStyles} from './UpcomingModalStyle';
+
+type UpcomingModalProps = {
+  showModal: boolean;
+  setShowModal: (value: boolean) => void;
+  text: string;
+};
+
+const UpcomingModal: React.FC<UpcomingModalProps> = ({
+  showModal,
+  setShowModal,
+  text,
+}) => {
+  return (
+    <Modal
+      isOpen={showModal}
+      onClose={() => {
+        setShowModal(false);
+      }}>
+      <ModalBackdrop />
+      <ModalContent style={UpcomingModalStyles.modalContent}>
+        <ModalBody>
+          <Text style={UpcomingModalStyles.modalText}>{text}</Text>
+        </ModalBody>
+        <ModalFooter>
+          <ModalCloseButton style={UpcomingModalStyles.closeButton}>
+            <Text style={UpcomingModalStyles.closeButtonText}>닫기</Text>
+          </ModalCloseButton>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default UpcomingModal;
