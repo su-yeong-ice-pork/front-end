8dd7748739a788f013b3b62f5cd2333706b350a8
taeyomi <terry11ty@naver.com>
2024-11-20 23:13:22 +0900

    refactor: Modal 폴더 생성하여 위치 인증 및 기능 추가 예정 모달 컴포넌트화

---
+++ src/components/Modal/LocationModal.tsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import UpcomingModal from './UpcomingModal';
+
+type LocationModalProps = {
+  showModal: boolean;
+  setShowModal: (value: boolean) => void;
+};
+
+const LocationModal: React.FC<LocationModalProps> = ({
+  showModal,
+  setShowModal,
+}) => {
+  return (
+    <UpcomingModal
+      showModal={showModal}
+      setShowModal={setShowModal}
+      text="위치 권한이 필요합니다."
+    />
+  );
+};
+
+export default LocationModal;
