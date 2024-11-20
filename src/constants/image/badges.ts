75b2cf1c3f99d25bbe7206816c98f1126e9ff5cf
kimjinwoo <rlawlsdn177@naver.com>
2024-11-20 20:41:59 +0900

    refactor: IMAGE 상수 선언

---
+++ src/constants/image/badges.ts
@@ -0,0 +1,8 @@
+export const BADGES = {
+  BADGE0: '@/assets/images/badge/badge0.png',
+  BADGE1: '@/assets/images/badge/badge1.png',
+  BADGE2: '@/assets/images/badge/badge2.png',
+  BADGE3: '@/assets/images/badge/badge3.png',
+  BADGE4: '@/assets/images/badge/badge4.png',
+  BADGE5: '@/assets/images/badge/badge5.png',
+};
---
+++ src/constants/image/emojis.ts
@@ -0,0 +1,8 @@
+export const EMOJIS = {
+  COMMENT: ' @/assets/images/emoji/comment.png',
+  HMM: ' @/assets/images/emoji/hmm.png',
+  SMILE: ' @/assets/images/emoji/smile.png',
+  SLEEPY_FACE_EMOJI: ' @/assets/images/emoji/sleepyFaceEmoji.png',
+  STAR: ' @/assets/images/emoji/star.png',
+  SWEAT: ' @/assets/images/emoji/sweat.png',
+};
