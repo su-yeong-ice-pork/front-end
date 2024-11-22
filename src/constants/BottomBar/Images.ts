195c02b943d841f0bbe915df7f66d5d7eb6cd702
kimjinwoo <rlawlsdn177@naver.com>
2024-11-22 13:00:46 +0900

    refactor: Bottom Bar에 사용되는 상수 선언

---
+++ src/constants/BottomBar/Images.ts
@@ -0,0 +1,21 @@
+export const PATH_NAME = {
+  LOG: 'Log',
+  STUDY: 'Study',
+  STUDY_DETAIL: 'StudyDetail',
+  HOME: 'Home',
+  ALARM: 'Alarm',
+  PROFILE: 'Profile',
+};
+
+export const IMAGE_PATH = {
+  NOTE_ON: require('@/assets/images/icons/noteOn.png'),
+  NOTE_OFF: require('@/assets/images/icons/note.png'),
+  STUDY_ON: require('@/assets/images/icons/studyOn.png'),
+  STUDY_OFF: require('@/assets/images/icons/study.png'),
+  HOME_ON: require('@/assets/images/icons/homeOn.png'),
+  HOME_OFF: require('@/assets/images/icons/home.png'),
+  ALARM_ON: require('@/assets/images/icons/alarmOn.png'),
+  ALARM_OFF: require('@/assets/images/icons/alarm.png'),
+  PROFILE_ON: require('@/assets/images/icons/profileOn.png'),
+  PROFILE_OFF: require('@/assets/images/icons/profile.png'),
+};
---
+++ src/constants/BottomBar/Messages.ts
@@ -0,0 +1,11 @@
+export const MESSAGES = {
+  MODAL: '추가 예정인 기능입니다.',
+};
+
+export const LABELS = {
+  HOME: '홈',
+  STUDY: '스터디',
+  LOG: '기록장',
+  ALARM: '알림',
+  PROFILE: '프로필',
+};
