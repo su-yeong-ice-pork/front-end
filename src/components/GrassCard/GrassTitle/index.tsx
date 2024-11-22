ced1adbcc2de3369fcb9ff24040e2831e91fafdb
이서현 <mellifluous3380@gmail.com>
2024-11-22 21:38:02 +0900

    fix: size constants 디렉토리 내부 선언하여 수정

--- src/components/GrassCard/GrassTitle/index.tsx
+++ src/components/GrassCard/GrassTitle/index.tsx
@@ -3,19 +3,21 @@ import {Box} from '@/components/ui/box';
 import {Text} from '@/components/ui/text';
 import {Heading} from '@/components/ui/heading';
 
+import {FONT_SIZE} from '@/src/constants/GrassCard/FontSizes';
+
 const GrassTitle = ({name, totalDays}) => {
   return (
     <Box style={{marginTop: 20}}>
       <Text>
-        <Heading size="xl">
-          <Text size="2xl" style={{color: '#009499'}}>
+        <Heading size={FONT_SIZE.XL}>
+          <Text size={FONT_SIZE['2XL']} style={{color: '#009499'}}>
             {name}
           </Text>
           님은 지금까지{'\n'}
         </Heading>
-        <Heading size="xl">
+        <Heading size={FONT_SIZE.XL}>
           총{' '}
-          <Text size="2xl" style={{color: '#009499'}}>
+          <Text size={FONT_SIZE['2XL']} style={{color: '#009499'}}>
             {totalDays}
           </Text>
           일의 잔디를 심으셨어요!
