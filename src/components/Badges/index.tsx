ee8275621820e7f9f99c22d7dc0c592115e28e69
taeyomi <terry11ty@naver.com>
2024-11-24 00:17:45 +0900

    refactor: 로그 삭제

--- src/components/Badges/index.tsx
+++ src/components/Badges/index.tsx
@@ -5,7 +5,6 @@ import BadgeText from './BadgeText';
 import BadgeView from './BadgeView';
 import {BadgesProps} from '@/src/api/badge/getBadgesPropsType';
 const Badges: React.FC<BadgesProps> = ({badges}) => {
-  console.log('BadgeView badges:', badges);
   return (
     <Box style={BadgeStyles.badgeContainer}>
       <BadgeText />
