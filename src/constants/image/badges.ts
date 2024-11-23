21a6c8bb666bdc805b0614e22c3a6861e7affb6a
taeyomi <terry11ty@naver.com>
2024-11-23 19:01:20 +0900

    refactor: constant 폴더의 badge 파일에 require추가

--- src/constants/image/badges.ts
+++ src/constants/image/badges.ts
@@ -1,8 +1,8 @@
-export const BADGES = {
-  BADGE0: '@/assets/images/badge/badge0.png',
-  BADGE1: '@/assets/images/badge/badge1.png',
-  BADGE2: '@/assets/images/badge/badge2.png',
-  BADGE3: '@/assets/images/badge/badge3.png',
-  BADGE4: '@/assets/images/badge/badge4.png',
-  BADGE5: '@/assets/images/badge/badge5.png',
-};
+export const BADGES = [
+  require('@/assets/images/badge/badge0.png'),
+  require('@/assets/images/badge/badge1.png'),
+  require('@/assets/images/badge/badge2.png'),
+  require('@/assets/images/badge/badge3.png'),
+  require('@/assets/images/badge/badge4.png'),
+  require('@/assets/images/badge/badge5.png'),
+];
