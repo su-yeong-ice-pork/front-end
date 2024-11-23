9856c427c20b048514979c5fe4ae02dc7380d7fe
taeyomi <terry11ty@naver.com>
2024-11-23 22:07:13 +0900

    refactor: BadgeView에서 Image와 Text를 gluestack ui로 변경

--- src/components/Badges/BadgeView/index.tsx
+++ src/components/Badges/BadgeView/index.tsx
@@ -2,9 +2,10 @@ import React, {useState, useEffect} from 'react';
 import {BadgeViewStyles} from './BadgeViewStyles';
 import {HStack} from '@/components/ui/hstack';
 import {Button, ButtonText} from '@/components/ui/button';
-import {Image, Text} from 'react-native';
 import {BadgesProps} from '@/src/api/badge/getBadgesPropsType';
 import BadgeModal from '../BadgeModal';
+import {Image} from '@/components/ui/image';
+import {Text} from '@/components/ui/text';
 import {BADGES} from '@/src/constants/image/badges';
 const BadgeView: React.FC<BadgesProps> = ({badges}) => {
   const [showModal, setShowModal] = useState(false);
@@ -15,6 +16,7 @@ const BadgeView: React.FC<BadgesProps> = ({badges}) => {
         <>
           {badges.slice(0, 3).map(badge => (
             <Image
+              alt="badge"
               key={badge.id}
               source={BADGES[Number(badge.fileName)] || BADGES[0]}
               style={BadgeViewStyles.badge}
