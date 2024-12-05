065ea93e3fd6874492417f1ec9016a91213a7320
taeyomi <terry11ty@naver.com>
2024-12-05 16:21:01 +0900

    style: 스타일 수정

--- src/components/Calendars/DateModal/dateModalStyles.ts
+++ src/components/Calendars/DateModal/dateModalStyles.ts
@@ -1,4 +1,4 @@
-import {StyleSheet, Dimensions} from 'react-native';
+import {StyleSheet} from 'react-native';
 import {FONT} from '@/src/constants/styles/font/default-font';
 
 export const DateModalStyles = StyleSheet.create({
--- src/components/Calendars/DateModal/index.tsx
+++ src/components/Calendars/DateModal/index.tsx
@@ -3,7 +3,7 @@ import {Text} from '@/components/ui/text';
 import moment from 'moment';
 import React from 'react';
 import {DateModalStyles} from './dateModalStyles';
-import {Modal} from 'react-native';
+import {Modal, ModalCloseButton} from '@/components/ui/modal';
 import {Button} from '@/components/ui/button';
 const DateModal = ({
   visible,
@@ -18,11 +18,10 @@ const DateModal = ({
 }) => {
   return (
     <Modal
-      visible={visible}
-      transparent={true}
-      animationType="slide"
-      onRequestClose={onClose}>
-      <Box style={DateModalStyles.modalBackground}>
+      isOpen={visible}
+      onClose={onClose}
+      style={DateModalStyles.modalBackground}>
+      <Box>
         <Box style={DateModalStyles.modalContainer}>
           <Text style={DateModalStyles.modalTitle}>
             {selectedDate
@@ -43,9 +42,9 @@ const DateModal = ({
           ) : (
             <Text>데이터가 없습니다.</Text>
           )}
-          <Button style={DateModalStyles.closeButton} onPress={onClose}>
+          <ModalCloseButton style={DateModalStyles.closeButton}>
             <Text style={DateModalStyles.closeButtonText}>닫기</Text>
-          </Button>
+          </ModalCloseButton>
         </Box>
       </Box>
     </Modal>
