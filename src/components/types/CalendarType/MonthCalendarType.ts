45fc52b0e8a82fbd54fee4b64aed65e7e72ee2fa
taeyomi <terry11ty@naver.com>
2024-12-05 17:36:05 +0900

    refactor: CustomDay를 컴포넌트로 분리

--- src/components/types/CalendarType/MonthCalendarType.ts
+++ src/components/types/CalendarType/MonthCalendarType.ts
@@ -1,7 +1,10 @@
 import {DateData} from 'react-native-calendars';
 
+type OnDayPress = (day: DateData) => void;
+
 export type MonthCalendarProps = {
   date: DateData;
   state: 'selected' | 'disabled';
   grassData: Record<string, {studyTime: number; grassScore: number}>;
+  onDayPress: OnDayPress;
 };
