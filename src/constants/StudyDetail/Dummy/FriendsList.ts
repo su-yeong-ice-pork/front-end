87790ebceefe88d660dc6686e55abc34129e14a9
kimjinwoo <rlawlsdn177@naver.com>
2024-12-09 21:45:45 +0900

    refactor: Dummy Data 상수로 이동 및 원 데이터 형태로 완전 복사

---
+++ src/constants/StudyDetail/Dummy/FriendsList.ts
@@ -0,0 +1,38 @@
+// 멤버 상세 조회 더미 데이터
+// GET: /friends
+export const DummyFriendsList = {
+  friends: [
+    {
+      id: 1,
+      name: '고민석',
+      profileImage: null,
+      message: 'Hello',
+      todayStudyTime: '04시간 33분',
+      studyStatus: true,
+    },
+    {
+      id: 2,
+      name: '유경미',
+      profileImage: null,
+      message: 'Hello World',
+      todayStudyTime: '12시간 33분',
+      studyStatus: false,
+    },
+    {
+      id: 3,
+      name: '김진우',
+      profileImage: null,
+      message: 'ㅋㅋㅋㅋㅋㅋㅋ 아 살기 힘드네',
+      todayStudyTime: '02시간 33분',
+      studyStatus: true,
+    },
+    {
+      id: 4,
+      name: '김태영',
+      profileImage: null,
+      message: '후....',
+      todayStudyTime: '01시간 33분',
+      studyStatus: false,
+    },
+  ],
+};
