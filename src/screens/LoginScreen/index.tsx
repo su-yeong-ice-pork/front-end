e52fa5d7927ba495e4db6df68f94ce6688aaad1c
won0104 <98508955+won0104@users.noreply.github.com>
2024-12-10 23:23:07 +0900

    refactor: index.tsx 파일 생성, GRADIENT 상수 사용

---
+++ src/screens/LoginScreen/index.tsx
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+
+import {LoginScreenStyles} from './LoginScreenStyle.ts';
+
+import LinearGradient from 'react-native-linear-gradient';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+import LoginForm from '../../components/LoginForm.tsx';
+import Loader from '../../components/Loader';
+import Slides from '../../components/Landing/Slides';
+
+import {COLOR} from '../../constants/styles/color/color';
+
+const LoginScreen = ({navigation}) => {
+    const [isLoading, setIsLoading] = useState(false);
+    return (
+        <LinearGradient
+            colors={COLOR.GRADIENT.TONE}
+            start={COLOR.GRADIENT.START}
+            end={COLOR.GRADIENT.END}
+            style={LoginScreenStyles.gradient}>
+            <KeyboardAwareScrollView
+                style={LoginScreenStyles.keyboardAwareScrollView}
+                contentContainerStyle={
+                    LoginScreenStyles.keyboardAwareScrollViewContent
+                }
+                enableOnAndroid={true}
+                extraScrollHeight={20}
+                keyboardShouldPersistTaps="handled"
+                bounces={false}>
+                <Slides/>
+
+                <LoginForm setIsLoading={setIsLoading}
+                />
+            </KeyboardAwareScrollView>
+            {isLoading && <Loader/>}
+        </LinearGradient>
+    );
+};
+
+export default LoginScreen;
