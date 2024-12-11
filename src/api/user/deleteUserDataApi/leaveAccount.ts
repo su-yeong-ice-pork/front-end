32c60f19acb019f589bfd40951aff654f5211f6c
kimjinwoo <rlawlsdn177@naver.com>
2024-12-12 02:13:42 +0900

    refactor: API 호출 코드 이동

---
+++ src/api/user/deleteUserDataApi/leaveAccount.ts
@@ -0,0 +1,27 @@
+import apiClient from '../../axiosInstance';
+
+interface LeaveAccountResponse {
+  success: boolean;
+  response: null;
+  error: null;
+}
+
+export const CancelAccount = async (
+  authToken: string,
+  password: string,
+): Promise<LeaveAccountResponse | null> => {
+  try {
+    const response = await apiClient.delete<LeaveAccountResponse>('/members', {
+      headers: {
+        Authorization: authToken,
+      },
+      data: {
+        password: password,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error deleting account:', error);
+    return null;
+  }
+};
