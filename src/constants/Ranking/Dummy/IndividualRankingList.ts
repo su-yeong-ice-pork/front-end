cacc4953175d267ceb6b94c2ff917d1ca0714b3e
won0104 <98508955+won0104@users.noreply.github.com>
2025-02-08 23:29:27 +0900

    feat : 필요한 더미 데이터 추가

---
+++ src/constants/Ranking/Dummy/IndividualRankingList.ts
@@ -0,0 +1,48 @@
+// 개인 랭킹 조회 더미 데이터
+//GET: /ranks/individual
+
+export const DummyRankingData = {
+  date: '9월 26일',
+  ranking: [
+    {
+      rank: 1,
+      memberId: 'user001',
+      name: '고민석',
+      profileImage: 'https://example.com/profile_hong.jpg',
+      totalStudyTime: '150시간 30분',
+      grassScore: 95,
+    },
+    {
+      rank: 2,
+      memberId: 'user002',
+      name: '유경미',
+      profileImage: 'https://example.com/profile_yu.jpg',
+      totalStudyTime: '120시간 10분',
+      grassScore: 88,
+    },
+    {
+      rank: 3,
+      memberId: 'user003',
+      name: '김진우',
+      profileImage: 'https://example.com/profile_kim.jpg',
+      totalStudyTime: '110시간 45분',
+      grassScore: 82,
+    },
+    {
+      rank: 4,
+      memberId: 'user004',
+      name: '김태영',
+      profileImage: 'https://example.com/profile_lee.jpg',
+      totalStudyTime: '105시간 20분',
+      grassScore: 80,
+    },
+    {
+      rank: 5,
+      memberId: 'user005',
+      name: '이민호',
+      profileImage: 'https://example.com/profile_lee_min.jpg',
+      totalStudyTime: '100시간 15분',
+      grassScore: 78,
+    },
+  ],
+};
