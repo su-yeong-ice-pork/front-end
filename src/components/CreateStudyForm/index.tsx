a88dafb1ed6cb1a073378ded6770d54dce75f917
kimjinwoo <rlawlsdn177@naver.com>
2025-02-10 14:38:29 +0900

    refactor: API 연결을 위한 구조 변경

--- src/components/CreateStudyForm/index.tsx
+++ src/components/CreateStudyForm/index.tsx
@@ -1,44 +1,25 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {VStack} from '@/components/ui';
 import CreateStudyInputField from '@/src/components/CreateStudyForm/InputField';
 import {ICONS} from '@/src/constants/image/icons';
-import {CREATE_FORM} from '@/src/constants/CreateStudy/createStudy.ts';
-import {CreateStudyFormStyles} from '@/src/components/CreateStudyForm/CreateStudyFormStyles.ts';
+import {CREATE_FORM} from '@/src/constants/CreateStudy/createStudy';
+import {CreateStudyFormStyles} from '@/src/components/CreateStudyForm/CreateStudyFormStyles';
 import SubmitButton from '@/src/components/CreateStudyForm/SubmitButton';
 import Introduction from '@/src/components/CreateStudyForm/Introduction';
-import {Text} from 'react-native';
+import {useCreateStudyForm} from '@/src/hooks/useCreateStudyForm';
 
-const CreateStudyForm = ({}) => {
-  const [name, setName] = useState('');
-  const [goalMessage, setGoalMessage] = useState('');
-  const [goalTime, setGoalTime] = useState('');
-  const [parsedGoalTime, setParsedGoalTime] = useState(0);
-  const [errorMessage, setErrorMessage] = useState('');
-
-  //임시 코드 - 생성 버튼 클릭 시 API 연결
-  const handleSubmit = () => {
-    console.log('Name:', name);
-    console.log('Goal Message:', goalMessage);
-    console.log('Goal Time:', parsedGoalTime); //parsedGoalTime 이 Int형입니당~
-  };
-
-  const handleGoalTimeChange = (text: string) => {
-    const parsedValue = parseInt(text, 10);
-    if (isNaN(parsedValue) || text.length > 4) {
-      setErrorMessage('시간은 4자리 이내의 숫자만 입력할 수 있습니다.');
-      setGoalTime('');
-    } else {
-      setErrorMessage('');
-      setGoalTime(text);
-      setParsedGoalTime(parsedValue);
-    }
-  };
-
-  const isFormValid =
-    name.trim() !== '' &&
-    goalMessage.trim() !== '' &&
-    !isNaN(parseInt(goalTime)) &&
-    parseInt(goalTime) >= 0;
+const CreateStudyForm = () => {
+  const {
+    name,
+    setName,
+    goalMessage,
+    setGoalMessage,
+    goalTime,
+    handleGoalTimeChange,
+    errorMessage,
+    isFormValid,
+    handleSubmit,
+  } = useCreateStudyForm();
 
   return (
     <VStack style={CreateStudyFormStyles.fullContainer}>
